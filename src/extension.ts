ee8cf660c1867c68ab5584279191f788e6121d63
Minieyes Chen <chenvey2@gmail.com>
2023-08-09 20:13:49 +0800

    add edit todo/programs/bookmarks.txt

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,24 @@
 import * as vscode from 'vscode';
+import * as utils from './Utils';
 import * as esFile from './ESFile';
 import * as bookmarks from './Bookmarks';
 
 export function activate(context: vscode.ExtensionContext) {
-    const esfileOpenFileDisposable = vscode.commands.registerCommand('minieyes.esfile.openfile', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('minieyes.esfile.openfile', () => {
         esFile.openFile();
-    });
-    const bookmarksOpenFileDisposable = vscode.commands.registerCommand('minieyes.bookmarks.openfile', () => {
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('minieyes.bookmarks.openfile', () => {
         bookmarks.openFile();
-    });
-
-    context.subscriptions.push(esfileOpenFileDisposable);
-    context.subscriptions.push(bookmarksOpenFileDisposable);
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('minieyes.edit.todo', () => {
+        utils.openFile('C:/Users/USER/OneDrive/文件/dendron/notes/TODO.md');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('minieyes.edit.programs', () => {
+        utils.openFile('C:/Users/USER/.config/bookmarks.txt');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('minieyes.edit.bookmarks', () => {
+        utils.openFile('C:/Users/USER/.config/programs.txt');
+    }));
 }
 
 export function deactivate() { }
