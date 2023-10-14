0b2737dad9423ecc4e5c1cfd93393d1707c1b503
Minieyes Chen <chenvey2@gmail.com>
2023-10-14 22:46:30 +0800

    use selectItemFromFile, selectItem from Utils

--- src/Bookmarks.ts
+++ src/Bookmarks.ts
@@ -2,17 +2,9 @@ import * as Utils from './Utils';
 import * as ESFile from './ESFile';
 import { window, workspace, Uri } from 'vscode';
 
-async function selectBookmark(bookmarks: Array<string>) {
-    const result = await window.showQuickPick(bookmarks.filter(bookmark => bookmark !== ''), {
-        placeHolder: 'Select Bookmark',
-    });
-    return result;
-}
-
 export async function openFile() {
     // select bookmark
-    const bookmarks: string[] = (await Utils.readFile(workspace.getConfiguration('minieyes.bookmarks').get<string>('path') as string)).split(/\r?\n/);
-    let bookmark = await selectBookmark(bookmarks);
+    let bookmark = await Utils.selectItemFromFile(workspace.getConfiguration('minieyes.bookmarks').get<string>('path') as string, 'Select Bookmark');
     if (!bookmark) { return; }
 
     // expand folders
@@ -20,10 +12,10 @@ export async function openFile() {
     const expandFolders = workspace.getConfiguration('minieyes.bookmarks').get<string[]>('expandFolders') ?? [];
     const forwardSlashFolders = expandFolders.map(path => path.replace('\\', '/'));
     while(forwardSlashFolders.includes(bookmark)) {
-        const subfolder = await selectBookmark(await Utils.readDir(bookmark));
+        const subfolder = await Utils.selectItem(await Utils.readDir(bookmark));
         if (!subfolder) { return; }
 
-        bookmark = bookmark + '/' + await selectBookmark(await Utils.readDir(bookmark));
+        bookmark = bookmark + '/' + await Utils.selectItem(await Utils.readDir(bookmark));
     }
 
     // pick file
